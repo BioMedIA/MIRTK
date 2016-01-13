45a8487bb1ce70c8669e048c5bd1538b46269ce9
Andreas Schuh <andreas.schuh.84@gmail.com>
2016-01-13 00:48:16 +0000

    cmake: Change version to 1.0.0rc2, add ChangeLog entry

--- BasisProject.cmake
+++ BasisProject.cmake
@@ -52,7 +52,7 @@ basis_project (
   # --------------------------------------------------------------------------
   # meta-data
   NAME         "MIRTK"
-  VERSION      "1.0.0rc1" # Version of core (+ external) modules
+  VERSION      "1.0.0rc2" # Version of core (+ external) modules
   SOVERSION    "0"        # API yet unstable
   AUTHORS      "Andreas Schuh"
   DESCRIPTION  "Medical Image Registration ToolKit"
