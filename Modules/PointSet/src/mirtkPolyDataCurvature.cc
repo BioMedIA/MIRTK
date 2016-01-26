876907f61b6772aa69992213beb481bea65c74f7
Andreas Schuh <andreas.schuh.84@gmail.com>
2016-01-26 03:13:26 +0000

    style: Fix code indentation [PointSet]

--- Modules/PointSet/src/mirtkPolyDataCurvature.cc
+++ Modules/PointSet/src/mirtkPolyDataCurvature.cc
@@ -87,8 +87,8 @@ class ComputeEdgeTensors
   vtkPolyData     *_Surface;
   vtkDataArray    *_Normals;
   const EdgeTable *_EdgeTable;
-  double               _DistNorm;
-  vtkDataArray        *_Tensors;
+  double           _DistNorm;
+  vtkDataArray    *_Tensors;
 
 public:
 
@@ -223,45 +223,45 @@ struct DecomposeTensors
   static void ComposeTensor(double T[6], EigenValues lambda, EigenVectors e, int i, int j, int k)
   {
     T[PolyDataCurvature::XX] = lambda(i) * e(i, i) * e(i, i)
-                                 + lambda(j) * e(i, j) * e(i, j)
-                                 + lambda(k) * e(i, k) * e(i, k);
+                             + lambda(j) * e(i, j) * e(i, j)
+                             + lambda(k) * e(i, k) * e(i, k);
     T[PolyDataCurvature::YY] = lambda(i) * e(j, i) * e(j, i)
-                                 + lambda(j) * e(j, j) * e(j, j)
-                                 + lambda(k) * e(j, k) * e(j, k);
+                             + lambda(j) * e(j, j) * e(j, j)
+                             + lambda(k) * e(j, k) * e(j, k);
     T[PolyDataCurvature::ZZ] = lambda(i) * e(k, i) * e(k, i)
-                                 + lambda(j) * e(k, j) * e(k, j)
-                                 + lambda(k) * e(k, k) * e(k, k);
+                             + lambda(j) * e(k, j) * e(k, j)
+                             + lambda(k) * e(k, k) * e(k, k);
     T[PolyDataCurvature::XY] = lambda(i) * e(i, i) * e(j, i)
-                                 + lambda(j) * e(i, j) * e(j, j)
-                                 + lambda(k) * e(i, k) * e(j, k);
+                             + lambda(j) * e(i, j) * e(j, j)
+                             + lambda(k) * e(i, k) * e(j, k);
     T[PolyDataCurvature::YZ] = lambda(i) * e(j, i) * e(k, i)
-                                 + lambda(j) * e(j, j) * e(k, j)
-                                 + lambda(k) * e(j, k) * e(k, k);
+                             + lambda(j) * e(j, j) * e(k, j)
+                             + lambda(k) * e(j, k) * e(k, k);
     T[PolyDataCurvature::XZ] = lambda(i) * e(i, i) * e(k, i)
-                                 + lambda(j) * e(i, j) * e(k, j)
-                                 + lambda(k) * e(i, k) * e(k, k);
+                             + lambda(j) * e(i, j) * e(k, j)
+                             + lambda(k) * e(i, k) * e(k, k);
   }
 
   static void ComposeTensor(double T[6], EigenValues lambda, double a[3], double b[3], double c[3], int i, int j, int k)
   {
     T[PolyDataCurvature::XX] = lambda(i) * a[0] * a[0]
-                                 + lambda(j) * b[0] * b[0]
-                                 + lambda(k) * c[0] * c[0];
+                             + lambda(j) * b[0] * b[0]
+                             + lambda(k) * c[0] * c[0];
     T[PolyDataCurvature::YY] = lambda(i) * a[1] * a[1]
-                                 + lambda(j) * b[1] * b[1]
-                                 + lambda(k) * c[1] * c[1];
+                             + lambda(j) * b[1] * b[1]
+                             + lambda(k) * c[1] * c[1];
     T[PolyDataCurvature::ZZ] = lambda(i) * a[2] * a[2]
-                                 + lambda(j) * b[2] * b[2]
-                                 + lambda(k) * c[2] * c[2];
+                             + lambda(j) * b[2] * b[2]
+                             + lambda(k) * c[2] * c[2];
     T[PolyDataCurvature::XY] = lambda(i) * a[0] * a[1]
-                                 + lambda(j) * b[0] * b[1]
-                                 + lambda(k) * c[0] * c[1];
+                             + lambda(j) * b[0] * b[1]
+                             + lambda(k) * c[0] * c[1];
     T[PolyDataCurvature::YZ] = lambda(i) * a[1] * a[2]
-                                 + lambda(j) * b[1] * b[2]
-                                 + lambda(k) * c[1] * c[2];
+                             + lambda(j) * b[1] * b[2]
+                             + lambda(k) * c[1] * c[2];
     T[PolyDataCurvature::XZ] = lambda(i) * a[0] * a[2]
-                                 + lambda(j) * b[0] * b[2]
-                                 + lambda(k) * c[0] * c[2];
+                             + lambda(j) * b[0] * b[2]
+                             + lambda(k) * c[0] * c[2];
   }
 
   void operator ()(const blocked_range<vtkIdType> &re) const
