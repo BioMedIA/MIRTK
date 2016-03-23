8d1ff691dd3373e47d207066ab4f52aa6f946792
Andreas Schuh <andreas.schuh.84@gmail.com>
2016-03-23 12:15:21 +0000

    fix: Add MSVC /bigobj option and use CMake BASIS GenerateExportHeader module

--- CMake/Modules/mirtkAddLibrary.cmake
+++ CMake/Modules/mirtkAddLibrary.cmake
@@ -24,6 +24,7 @@ endif ()
 # ------------------------------------------------------------------------------
 ## Add MIRTK module library
 function(mirtk_add_library)
+  # Parse arguments
   if (NOT PROJECT_NAME)
     message(FATAL_ERROR "mirtk_add_library called outside project scope!")
   endif ()
@@ -67,8 +68,10 @@ function(mirtk_add_library)
   if (TARGET_AUTO_REGISTER AND BUILD_SHARED_LIBS)
     target_compile_definitions(${target_uid} PRIVATE MIRTK_AUTO_REGISTER)
   endif ()
-  # Generate export header file and set WINDOWS_EXPORT_ALL_SYMBOLS target property
-  include(GenerateExportHeader)
+  # VTK 7.0.0 adds its CMake modules directory to CMAKE_MODULE_PATH
+  # and this directory contains a GenerateExportHeader.cmake module
+  include("${BASIS_MODULE_PATH}/GenerateExportHeader.cmake")
+  # Generate MIRTK<Module>Export.h file
   generate_export_header(${target_uid}
     PREFIX_NAME              "MIRTK_"
     BASE_NAME                "${PROJECT_NAME}"
@@ -78,64 +81,60 @@ function(mirtk_add_library)
     NO_DEPRECATED_MACRO_NAME "${PROJECT_NAME}_NO_DEPRECATED"
     STATIC_DEFINE            "${PROJECT_NAME}_STATIC_DEFINE"
     EXPORT_FILE_NAME         "${BINARY_INCLUDE_DIR}/mirtk${PROJECT_NAME}Export.h"
-    DEFINE_NO_DEPRECATED
   )
-  if (WIN32 AND BUILD_SHARED_LIBS)
-    if (CMAKE_VERSION VERSION_LESS 3.4)
-      message(FATAL_ERROR "Build of DLLs on Windows (BUILD_SHARED_LIBS=ON) requires CMake version 3.4 or greater!")
+  if (BUILD_SHARED_LIBS)
+    if (WIN32)
+      if (CMAKE_VERSION VERSION_LESS 3.4)
+        message(FATAL_ERROR "Build of DLLs on Windows (BUILD_SHARED_LIBS=ON) requires CMake version 3.4 or greater!")
+      endif ()
+      set_target_properties(${target_uid} PROPERTIES WINDOWS_EXPORT_ALL_SYMBOLS TRUE)
     endif ()
-    set_target_properties(${target_uid} PROPERTIES WINDOWS_EXPORT_ALL_SYMBOLS TRUE)
+    if (MSVC)
+      target_compile_options(${target_uid} PRIVATE /bigobj)
+    endif ()
+  else ()
+    target_compile_definitions(${target_uid} PRIVATE MIRTK_${PROJECT_NAME}_STATIC_DEFINE)
   endif ()
   # Add link dependencies and record all directories containing
   # external/imported library files for [DY]LD_LIBRARY_PATH
   # setting in "mirtk" command execution script
-  if (NOT COMMAND mirtk_get_target_name)
-    include("${MIRTK_MODULE_PATH}/mirtkGetTargetName.cmake")
-  endif ()
-  set(keyword)
-  foreach (dep IN LISTS TARGET_DEPENDS)
-    if (dep MATCHES "^(debug|optimized|general)$")
-      set(keyword ${dep})
-    else ()
-      mirtk_get_target_name(dep_name ${dep})
-      if (TARGET ${dep_name})
-        get_property(type TARGET ${dep_name} PROPERTY TYPE)
-        if (type MATCHES LIBRARY)
-          target_link_libraries(${target_name} ${keyword} ${dep_name})
-          get_property(is_imported TARGET ${dep_name} PROPERTY IMPORTED)
-          if (is_imported)
-            basis_get_target_location(dep_path ${dep_name} PATH)
-            if (dep_path)
-              set_property(GLOBAL APPEND PROPERTY MIRTK_LIBRARY_PATH ${dep_path})
+  if (TARGET_DEPENDS)
+    if (NOT COMMAND mirtk_get_target_name)
+      include("${MIRTK_MODULE_PATH}/mirtkGetTargetName.cmake")
+    endif ()
+    set(keyword)
+    foreach (dep IN LISTS TARGET_DEPENDS)
+      if (dep MATCHES "^(debug|optimized|general)$")
+        set(keyword ${dep})
+      else ()
+        mirtk_get_target_name(dep_name ${dep})
+        if (TARGET ${dep_name})
+          get_property(type TARGET ${dep_name} PROPERTY TYPE)
+          if (type MATCHES LIBRARY)
+            target_link_libraries(${target_name} ${keyword} ${dep_name})
+            get_property(is_imported TARGET ${dep_name} PROPERTY IMPORTED)
+            if (is_imported)
+              basis_get_target_location(dep_path ${dep_name} PATH)
+              if (dep_path)
+                set_property(GLOBAL APPEND PROPERTY MIRTK_LIBRARY_PATH ${dep_path})
+              endif ()
             endif ()
+          else ()
+            add_dependencies(${target_name} ${dep_name})
           endif ()
         else ()
-          add_dependencies(${target_name} ${dep_name})
+          target_link_libraries(${target_name} ${keyword} ${dep_name})
+          if (IS_ABSOLUTE "${dep_name}")
+            get_filename_component(dep_path "${dep_name}" PATH)
+            set_property(GLOBAL APPEND PROPERTY MIRTK_LIBRARY_PATH ${dep_path})
+          endif ()
         endif ()
-      else ()
-        target_link_libraries(${target_name} ${keyword} ${dep_name})
-        if (IS_ABSOLUTE "${dep_name}")
-          get_filename_component(dep_path "${dep_name}" PATH)
-          set_property(GLOBAL APPEND PROPERTY MIRTK_LIBRARY_PATH ${dep_path})
-        endif ()
+        set(keyword)
       endif ()
-      set(keyword)
-    endif ()
-  endforeach ()
-  # Remove duplicates from MIRTK_LIBRARY_PATH
-  get_property(ldpath GLOBAL PROPERTY MIRTK_LIBRARY_PATH)
-  list(REMOVE_DUPLICATES ldpath)
-  set_property(GLOBAL PROPERTY MIRTK_LIBRARY_PATH "${ldpath}")
-  include(GenerateExportHeader)
-  generate_export_header(${target_uid}
-    PREFIX_NAME              "MIRTK_"
-    BASE_NAME                "${PROJECT_NAME}"
-    EXPORT_MACRO_NAME        "${PROJECT_NAME}_EXPORT"
-    NO_EXPORT_MACRO_NAME     "${PROJECT_NAME}_NO_EXPORT"
-    DEPRECATED_MACRO_NAME    "${PROJECT_NAME}_DEPRECATED"
-    NO_DEPRECATED_MACRO_NAME "${PROJECT_NAME}_NO_DEPRECATED"
-    STATIC_DEFINE            "${PROJECT_NAME}_STATIC_DEFINE"
-    EXPORT_FILE_NAME         "${BINARY_INCLUDE_DIR}/mirtk${PROJECT_NAME}Export.h"
-    DEFINE_NO_DEPRECATED
-  )
+    endforeach ()
+    # Remove duplicates from MIRTK_LIBRARY_PATH
+    get_property(ldpath GLOBAL PROPERTY MIRTK_LIBRARY_PATH)
+    list(REMOVE_DUPLICATES ldpath)
+    set_property(GLOBAL PROPERTY MIRTK_LIBRARY_PATH "${ldpath}")
+  endif ()
 endfunction()
