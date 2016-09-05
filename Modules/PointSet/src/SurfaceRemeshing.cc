601960832dfd67ac423c572d105afbaf7886ecde
Andreas Schuh <andreas.schuh.84@gmail.com>
2016-09-05 16:39:37 +0100

    fix: IsBoundaryEdge check in SurfaceRemeshing filter [PointSet]

--- Modules/PointSet/src/SurfaceRemeshing.cc
+++ Modules/PointSet/src/SurfaceRemeshing.cc
@@ -244,7 +244,11 @@ inline vtkIdType SurfaceRemeshing::GetCellEdgeNeighbor(vtkIdType cellId, vtkIdTy
   vtkIdType neighborCellId = -1;
   for (vtkIdType i = 0; i < cellIds->GetNumberOfIds(); ++i) {
     if (_Output->GetCellType(cellIds->GetId(i)) != VTK_EMPTY_CELL) {
-      if (neighborCellId != -1) return -1; // should not happen
+      if (neighborCellId != -1) {
+        // may happen for boundary edges, i.e., edges with either only one
+        // or more than two adjacent cells
+        return -1;
+      }
       neighborCellId = cellIds->GetId(i);
     }
   }
@@ -278,18 +282,15 @@ inline bool SurfaceRemeshing::IsBoundaryPoint(vtkIdType ptId) const
 // -----------------------------------------------------------------------------
 inline bool SurfaceRemeshing::IsBoundaryEdge(vtkIdType ptId1, vtkIdType ptId2) const
 {
-  vtkIdType *cells1, *cells2;
+  vtkIdType      *cells1, *cells2;
   unsigned short ncells1, ncells2, n = 0;
   _Output->GetPointCells(ptId1, ncells1, cells1);
   _Output->GetPointCells(ptId2, ncells2, cells2);
   for (unsigned short i = 0; i < ncells1; ++i)
   for (unsigned short j = 0; j < ncells2; ++j) {
-    if (cells1[i] == cells2[j]) {
-      ++n;
-      break;
-    }
+    if (cells1[i] == cells2[j]) ++n;
   }
-  return n < 2;
+  return n != 2;
 }
 
 // -----------------------------------------------------------------------------
