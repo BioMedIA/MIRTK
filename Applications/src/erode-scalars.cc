10a6837f9bffa55cdce3a5195af43887e7fcda55
Andreas Schuh <andreas.schuh.84@gmail.com>
2016-11-18 00:11:06 +0000

    enh: Enable morphological operations also on cell data [Applications]

--- Applications/src/erode-scalars.cc
+++ Applications/src/erode-scalars.cc
@@ -24,6 +24,7 @@
 #include "mirtk/EdgeTable.h"
 #include "mirtk/EdgeConnectivity.h"
 #include "mirtk/ErodePointData.h"
+#include "mirtk/ErodeCellData.h"
 
 
 using namespace mirtk;
@@ -40,10 +41,9 @@ void PrintHelp(const char *name)
   cout << "Usage: " << name << " <input> <output> [options]\n";
   cout << "\n";
   cout << "Description:\n";
-  cout << "  Erodes scalar point data of an input point set by replacing a point's\n";
-  cout << "  value by the minimum of the values of its neighboring points. When the\n";
-  cout << "  input data array has more than one component, each component is processed\n";
-  cout << "  separately.\n";
+  cout << "  Erodes scalar data of an input point set by replacing a value by the\n";
+  cout << "  minimum of the adjacent data values. When the input data array has\n";
+  cout << "  more than one component, each component is processed separately.\n";
   cout << "\n";
   cout << "Arguments:\n";
   cout << "  input    Input  point set.\n";
@@ -51,7 +51,13 @@ void PrintHelp(const char *name)
   cout << "\n";
   cout << "Optional arguments:\n";
   cout << "  -a, -array, -scalars <name>\n";
-  cout << "      Name of point data array. (default: active scalars)\n";
+  cout << "      Name of :option:`-point-data` or :option:`-cell-data` array. (default: active scalars)\n";
+  cout << "  -point-data, -pd [<name>]\n";
+  cout << "      Process point data array. (default)\n";
+  cout << "  -cell-data, -cd [<name>]\n";
+  cout << "      Process cell data array.\n";
+  cout << "  -o, -output-array, -output-scalars <name>\n";
+  cout << "      Name of output data array. (default: overwrite input array)\n";
   cout << "  -c, -connectivity, -neighborhood <int>\n";
   cout << "      Maximum node edge-connectivity of neighbors. (default: 1)\n";
   cout << "  -r, -radius <float>\n";
@@ -85,14 +91,23 @@ int main(int argc, char *argv[])
   const char *array_name        = nullptr;
   const char *output_array_name = nullptr;
 
-  int iterations    = 1;
-  int connectivity  = 1;
+  bool process_pd   = true;
+  int  iterations   = 1;
+  int  connectivity = 1;
   double     radius = 0.;
   FileOption fopt   = FO_Default;
 
   for (ALL_OPTIONS) {
     if (OPTION("-a") || OPTION("-array") || OPTION("-scalars") || OPTION("-name")) {
       array_name = ARGUMENT;
+    }
+    else if (OPTION("-pd") || OPTION("-point-data")) {
+      process_pd = true;
+      if (HAS_ARGUMENT) array_name = ARGUMENT;
+    }
+    else if (OPTION("-cd") || OPTION("-cell-data")) {
+      process_pd = false;
+      if (HAS_ARGUMENT) array_name = ARGUMENT;
     }
     else if (OPTION("-o") || OPTION("-output-array") || OPTION("-output-scalars") || OPTION("-output-name")) {
       output_array_name = ARGUMENT;
@@ -114,32 +129,50 @@ int main(int argc, char *argv[])
   vtkSmartPointer<vtkPolyData> input = ReadPolyData(input_name);
   if (verbose) cout << " done" << endl;
 
-  SharedPtr<EdgeTable> edges(new EdgeTable(input));
-  SharedPtr<EdgeConnectivity> neighbors;
-  if (radius > 0.) {
-    neighbors = NewShared<EdgeConnectivity>(input, radius, edges.get());
-  } else {
-    neighbors = NewShared<EdgeConnectivity>(input, connectivity, edges.get());
-  }
-  if (verbose) cout << "Eroding with c=" << neighbors->Maximum() << "...", cout.flush();
-  ErodePointData filter;
-  filter.Input(input);
-  filter.DataName(array_name);
-  filter.EdgeTable(edges);
-  filter.Neighbors(neighbors);
-  filter.Connectivity(connectivity);
-  filter.Radius(radius);
-  filter.Iterations(iterations);
-  filter.Run();
-  if (output_array_name && (!array_name || strcmp(array_name, output_array_name) != 0)) {
-    filter.OutputData()->SetName(output_array_name);
-    if (filter.InputData()->GetName()) {
-      filter.Output()->GetPointData()->AddArray(filter.InputData());
+  if (process_pd) {
+    SharedPtr<EdgeTable> edges(new EdgeTable(input));
+    SharedPtr<EdgeConnectivity> neighbors;
+    if (radius > 0.) {
+      neighbors = NewShared<EdgeConnectivity>(input, radius, edges.get());
+    } else {
+      neighbors = NewShared<EdgeConnectivity>(input, connectivity, edges.get());
     }
+    if (verbose) cout << "Eroding point data with c=" << neighbors->Maximum() << "...", cout.flush();
+    ErodePointData filter;
+    filter.Input(input);
+    filter.DataName(array_name);
+    filter.EdgeTable(edges);
+    filter.Neighbors(neighbors);
+    filter.Connectivity(connectivity);
+    filter.Radius(radius);
+    filter.Iterations(iterations);
+    filter.Run();
+    if (output_array_name && (!array_name || strcmp(array_name, output_array_name) != 0)) {
+      filter.OutputData()->SetName(output_array_name);
+      if (filter.InputData()->GetName()) {
+        filter.Output()->GetPointData()->AddArray(filter.InputData());
+      }
+    }
+    input = filter.Output();
+    if (verbose) cout << " done" << endl;
+  } else {
+    if (verbose) cout << "Eroding cell data...", cout.flush();
+    ErodeCellData filter;
+    filter.Input(input);
+    filter.DataName(array_name);
+    filter.Iterations(iterations);
+    filter.Run();
+    if (output_array_name && (!array_name || strcmp(array_name, output_array_name) != 0)) {
+      filter.OutputData()->SetName(output_array_name);
+      if (filter.InputData()->GetName()) {
+        filter.Output()->GetCellData()->AddArray(filter.InputData());
+      }
+    }
+    input = filter.Output();
+    if (verbose) cout << " done" << endl;
   }
-  if (verbose) cout << " done" << endl;
 
-  if (!WritePolyData(output_name, filter.Output(), fopt)) {
+  if (!WritePolyData(output_name, input, fopt)) {
     FatalError("Failed to write result to " << output_name << "!");
   }
 
