5ab9cefaedfab3480806ebf82f41ba24846ddde0
Andreas Schuh <andreas.schuh.84@gmail.com>
2017-08-02 15:39:12 +0100

    enh: Add separable convolution filter, cubic B-spline filter [Image]

--- Modules/Image/src/GaussianBlurring.cc
+++ Modules/Image/src/GaussianBlurring.cc
@@ -21,13 +21,8 @@
 #include "mirtk/GaussianBlurring.h"
 
 #include "mirtk/Math.h"
-#include "mirtk/Memory.h"
-#include "mirtk/GenericImage.h"
-#include "mirtk/ConvolutionFunction.h"
 #include "mirtk/ScalarFunctionToImage.h"
 #include "mirtk/ScalarGaussian.h"
-#include "mirtk/Profiling.h"
-#include "mirtk/Deallocate.h"
 
 
 namespace mirtk {
@@ -37,11 +32,7 @@ namespace mirtk {
 template <class VoxelType>
 GaussianBlurring<VoxelType>::GaussianBlurring(double sigma)
 :
-  _SigmaX(sigma),
-  _SigmaY(sigma),
-  _SigmaZ(sigma),
-  _SigmaT(.0),
-  _Kernel(NULL)
+  GaussianBlurring<VoxelType>::GaussianBlurring(sigma, sigma, sigma, 0.)
 {
 }
 
@@ -52,8 +43,7 @@ GaussianBlurring<VoxelType>::GaussianBlurring(double xsigma, double ysigma, doub
   _SigmaX(xsigma),
   _SigmaY(ysigma),
   _SigmaZ(zsigma),
-  _SigmaT(tsigma),
-  _Kernel(NULL)
+  _SigmaT(tsigma)
 {
 }
 
@@ -61,7 +51,6 @@ GaussianBlurring<VoxelType>::GaussianBlurring(double xsigma, double ysigma, doub
 template <class VoxelType>
 GaussianBlurring<VoxelType>::~GaussianBlurring()
 {
-  Delete(_Kernel);
 }
 
 // -----------------------------------------------------------------------------
@@ -69,7 +58,7 @@ template <class VoxelType>
 void GaussianBlurring<VoxelType>::SetSigma(double sigma)
 {
   _SigmaX = _SigmaY = _SigmaZ = sigma;
-  _SigmaT = .0;
+  _SigmaT = 0.;
 }
 
 // -----------------------------------------------------------------------------
@@ -84,17 +73,6 @@ void GaussianBlurring<VoxelType>::SetSigma(double xsigma, double ysigma, double 
 
 // -----------------------------------------------------------------------------
 template <class VoxelType>
-void GaussianBlurring<VoxelType>::Initialize()
-{
-  // Initialize base class
-  ImageToImage<VoxelType>::Initialize();
-
-  // Instantiate convolution kernel of proper image type
-  _Kernel = new GenericImage<RealPixel>;
-}
-
-// -----------------------------------------------------------------------------
-template <class VoxelType>
 int GaussianBlurring<VoxelType>::KernelSize(double sigma)
 {
   return 2 * ifloor(3.0 * sigma) + 1;
@@ -102,186 +80,59 @@ int GaussianBlurring<VoxelType>::KernelSize(double sigma)
 
 // -----------------------------------------------------------------------------
 template <class VoxelType>
-void GaussianBlurring<VoxelType>::InitializeKernel(double sigma)
+UniquePtr<typename GaussianBlurring<VoxelType>::KernelType>
+GaussianBlurring<VoxelType>::InitializeKernel(double sigma)
 {
   // Create filter kernel for 1D Gaussian function
-  _Kernel->Initialize(KernelSize(sigma), 1, 1);
+  UniquePtr<KernelType> kernel(new KernelType(KernelSize(sigma), 1, 1));
 
   // Create scalar function which corresponds to a 1D Gaussian function
-  ScalarGaussian gaussian(sigma,            .0, .0,  // stddev in x, y, z
-                          _Kernel->X() / 2, .0, .0); // center in x, y, z
+  ScalarGaussian gaussian(sigma,           0., 0.,  // stddev in x, y, z
+                          kernel->X() / 2, 0., 0.); // center in x, y, z
 
   // Convert from scalar function to filter kernel
-  RealPixel *p = _Kernel->Data();
-  for (int i = 0; i < _Kernel->X(); ++i, ++p) {
-    (*p) = gaussian.Evaluate(i);
+  for (int i = 0; i < kernel->X(); ++i) {
+    kernel->PutAsDouble(i, gaussian.Evaluate(i));
   }
+
+  return kernel;
 }
 
 // -----------------------------------------------------------------------------
 template <class VoxelType>
-void GaussianBlurring<VoxelType>::Run()
+void GaussianBlurring<VoxelType>::Initialize()
 {
-  MIRTK_START_TIMING();
+  // Initialize base class
+  SeparableConvolution<VoxelType>::Initialize();
 
-  // Do the initial set up
-  this->Initialize();
-
-  GenericImage<VoxelType> *input  = const_cast<GenericImage<VoxelType> *>(this->Input());
-  GenericImage<VoxelType> *output = this->Output();
-
-  const ImageAttributes &attr = input->Attributes();
-  const int N = (AreEqual(attr._dt, 0.) ? attr._t : 1);
-
-  // Blur along x axis
+  // Instantiate convolution kernels
+  const ImageType *input  = this->Input();
   if (!AreEqual(_SigmaX, 0.) && input->X() > 1) {
-    if (output == this->Input()) output = new GenericImage<VoxelType>(attr);
-    this->InitializeKernel(_SigmaX / input->XSize());
-    for (int n = 0; n < N; ++n) {
-      using ConvolutionFunction::ConvolveInX;
-      ConvolveInX<RealPixel> conv(input, _Kernel->Data(), _Kernel->X(), true, n);
-      ParallelForEachVoxel(attr, input, output, conv);
-    }
-    swap(input, output);
+    _GaussianKernel[0] = this->InitializeKernel(_SigmaX / input->XSize());
+  } else {
+    _GaussianKernel[0].reset();
   }
-
-  // Blur along y axis
   if (!AreEqual(_SigmaY, 0.) && input->Y() > 1) {
-    if (output == this->Input()) output = new GenericImage<VoxelType>(attr);
-    this->InitializeKernel(_SigmaY / input->YSize());
-    for (int n = 0; n < N; ++n) {
-      using ConvolutionFunction::ConvolveInY;
-      ConvolveInY<RealPixel> conv(input, _Kernel->Data(), _Kernel->X(), true, n);
-      ParallelForEachVoxel(attr, input, output, conv);
-    }
-    swap(input, output);
+    _GaussianKernel[1] = this->InitializeKernel(_SigmaY / input->YSize());
+  } else {
+    _GaussianKernel[1].reset();
+  }
+  if (!AreEqual(_SigmaZ, 0.) && input->Z() > 1 && !AreEqual(input->ZSize(), 0.)) {
+    _GaussianKernel[2] = this->InitializeKernel(_SigmaZ / input->ZSize());
+  } else {
+    _GaussianKernel[2].reset();
+  }
+  if (!AreEqual(_SigmaT, 0.) && input->T() > 1 && !AreEqual(input->TSize(), 0.)) {
+    _GaussianKernel[3] = this->InitializeKernel(_SigmaT / input->TSize());
+  } else {
+    _GaussianKernel[3].reset();
   }
 
-  // Blur along z axis
-  if (!AreEqual(_SigmaZ, 0.) && input->Z() > 1) {
-    if (output == this->Input()) output = new GenericImage<VoxelType>(attr);
-    this->InitializeKernel(_SigmaZ / input->ZSize());
-    for (int n = 0; n < N; ++n) {
-      using ConvolutionFunction::ConvolveInZ;
-      ConvolveInZ<RealPixel> conv(input, _Kernel->Data(), _Kernel->X(), true, n);
-      ParallelForEachVoxel(attr, input, output, conv);
-    }
-    swap(input, output);
-  }
-
-  // Blur along t axis
-  if (!AreEqual(_SigmaT, 0.) && input->T() > 1) {
-    if (output == this->Input()) output = new GenericImage<VoxelType>(attr);
-    this->InitializeKernel(_SigmaT / input->TSize());
-    using ConvolutionFunction::ConvolveInT;
-    ConvolveInT<RealPixel> conv(input, _Kernel->Data(), _Kernel->X(), true);
-    ParallelForEachVoxel(attr, input, output, conv);
-    swap(input, output);
-  }
-
-  // Copy result if last output (i.e., after swap input pointer) was not filter
-  // output image and delete possibly additionally allocated image
-  if (input != output) {
-    if (input == this->Output()) {
-      if (output != this->Input()) Delete(output);
-    } else {
-      this->Output()->CopyFrom(input->Data());
-      if (input != this->Input()) Delete(input);
-    }
-  }
-
-  // Do the final cleaning up
-  this->Finalize();
-
-  MIRTK_DEBUG_TIMING(5, this->NameOfClass());
-}
-
-// -----------------------------------------------------------------------------
-template <class VoxelType>
-void GaussianBlurring<VoxelType>::Finalize()
-{
-  // Finalize base class
-  ImageToImage<VoxelType>::Finalize();
-
-  // Copy background information
-  if (this->Input() != this->Output()) {
-    if (this->Input()->HasBackgroundValue()) {
-      this->Output()->PutBackgroundValueAsDouble(this->Input()->GetBackgroundValueAsDouble());
-    }
-    if (this->Input()->HasMask()) {
-      this->Output()->PutMask(new BinaryImage(*this->Input()->GetMask()), true);
-    }
-  }
-
-  // Free convolution kernel
-  Delete(_Kernel);
-}
-
-// -----------------------------------------------------------------------------
-template <class VoxelType>
-void GaussianBlurring<VoxelType>::RunX()
-{
-  const double ysigma = _SigmaY;
-  const double zsigma = _SigmaZ;
-  const double tsigma = _SigmaT;
-
-  _SigmaY = _SigmaZ = _SigmaT = .0;
-  this->Run();
-
-  _SigmaY = ysigma;
-  _SigmaZ = zsigma;
-  _SigmaT = tsigma;
-}
-
-// -----------------------------------------------------------------------------
-template <class VoxelType>
-void GaussianBlurring<VoxelType>::RunY()
-{
-  const double xsigma = _SigmaX;
-  const double zsigma = _SigmaZ;
-  const double tsigma = _SigmaT;
-
-  _SigmaX = _SigmaZ = _SigmaT = .0;
-  this->Run();
-
-  _SigmaX = xsigma;
-  _SigmaZ = zsigma;
-  _SigmaT = tsigma;
-}
-
-// -----------------------------------------------------------------------------
-template <class VoxelType>
-void GaussianBlurring<VoxelType>::RunZ()
-{
-  const double xsigma = _SigmaX;
-  const double ysigma = _SigmaY;
-  const double tsigma = _SigmaT;
-
-  _SigmaX = _SigmaY = _SigmaT = .0;
-  this->Run();
-
-  _SigmaX = xsigma;
-  _SigmaY = ysigma;
-  _SigmaT = tsigma;
-}
-
-// -----------------------------------------------------------------------------
-template <class VoxelType>
-void GaussianBlurring<VoxelType>::RunT()
-{
-  const double xsigma = _SigmaX;
-  const double ysigma = _SigmaY;
-  const double zsigma = _SigmaZ;
-  const double tsigma = _SigmaT;
-
-  _SigmaX = _SigmaY = _SigmaZ = .0;
-  if (_SigmaT == .0) _SigmaT = _SigmaX;
-  this->Run();
-
-  _SigmaX = xsigma;
-  _SigmaY = ysigma;
-  _SigmaZ = zsigma;
-  _SigmaT = tsigma;
+  // Set kernels
+  this->_KernelX = _GaussianKernel[0].get();
+  this->_KernelY = _GaussianKernel[1].get();
+  this->_KernelZ = _GaussianKernel[2].get();
+  this->_KernelT = _GaussianKernel[3].get();
 }
 
 // =============================================================================
---
+++ Modules/Image/src/SeparableConvolution.cc
@@ -0,0 +1,359 @@
+/*
+ * Medical Image Registration ToolKit (MIRTK)
+ *
+ * Copyright 2017 Imperial College London
+ * Copyright 2017 Andreas Schuh
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "mirtk/SeparableConvolution.h"
+
+#include "mirtk/ConvolutionFunction.h"
+#include "mirtk/Profiling.h"
+
+
+namespace mirtk {
+
+
+// -----------------------------------------------------------------------------
+template <class TVoxel, class TKernel>
+SeparableConvolution<TVoxel, TKernel>::SeparableConvolution(const KernelType *k)
+:
+  SeparableConvolution<TVoxel, TKernel>(k, k, k, k)
+{
+}
+
+// -----------------------------------------------------------------------------
+template <class TVoxel, class TKernel>
+SeparableConvolution<TVoxel, TKernel>
+::SeparableConvolution(const KernelType *kx, const KernelType *ky, const KernelType *kz, const KernelType *kt)
+:
+  _KernelX(kx),
+  _KernelY(ky),
+  _KernelZ(kz),
+  _KernelT(kt),
+  _Normalize(true),
+  _UseBackgroundMask(false),
+  _UseBackgroundValue(false),
+  _UsePaddingValue(false),
+  _PaddingValue(NaN)
+{
+}
+
+// -----------------------------------------------------------------------------
+template <class TVoxel, class TKernel>
+SeparableConvolution<TVoxel, TKernel>::~SeparableConvolution()
+{
+}
+
+// -----------------------------------------------------------------------------
+template <class TVoxel, class TKernel>
+void SeparableConvolution<TVoxel, TKernel>::Kernel(const KernelType *k)
+{
+  _KernelX = _KernelY = _KernelZ = _KernelT = k;
+}
+
+// -----------------------------------------------------------------------------
+template <class TVoxel, class TKernel>
+bool SeparableConvolution<TVoxel, TKernel>::CheckKernel(const KernelType *k) const
+{
+  if (k == nullptr) return false;
+  if (k->NumberOfVoxels() == 0) return false;
+  return (k->Y() == 1 || k->Z() == 1 || k->T() == 1);
+}
+
+// -----------------------------------------------------------------------------
+template <class TVoxel, class TKernel>
+void SeparableConvolution<TVoxel, TKernel>::Initialize()
+{
+  // Initialize base class
+  ImageToImage<TVoxel>::Initialize();
+
+  // Check arguments
+  if (_KernelX && !CheckKernel(_KernelX)) {
+    Throw(ERR_InvalidArgument, __FUNCTION__, "Convolution kernel along x axis is invalid");
+  }
+  if (_KernelY && !CheckKernel(_KernelY)) {
+    Throw(ERR_InvalidArgument, __FUNCTION__, "Convolution kernel along y axis is invalid");
+  }
+  if (_KernelZ && !CheckKernel(_KernelZ)) {
+    Throw(ERR_InvalidArgument, __FUNCTION__, "Convolution kernel along z axis is invalid");
+  }
+  if (_KernelT && !CheckKernel(_KernelT)) {
+    Throw(ERR_InvalidArgument, __FUNCTION__, "Convolution kernel along t axis is invalid");
+  }
+}
+
+// -----------------------------------------------------------------------------
+template <class TVoxel, class TKernel>
+void SeparableConvolution<TVoxel, TKernel>::Run()
+{
+  MIRTK_START_TIMING();
+
+  // Do the initial set up
+  this->Initialize();
+
+  ImageType *input  = const_cast<ImageType *>(this->Input());
+  ImageType *output = this->Output();
+
+  const ImageAttributes &attr = input->Attributes();
+
+  // Number of vector components
+  int N = 1;
+  if (attr._t == 1) {
+    if (attr._z > 1 && AreEqual(attr._dz, 0.)) {
+      N = attr._z;
+    }
+  } else if (attr._t > 1 && AreEqual(attr._dt, 0.)) {
+    N = attr._t;
+  }
+
+  int    padmode = 0;
+  double padding = _PaddingValue;
+  if (_UseBackgroundMask && input->HasMask()) {
+    padmode = 1;
+  } else if (_UseBackgroundValue && input->HasBackgroundValue()) {
+    padding = input->GetBackgroundValueAsDouble();
+    padmode = 2;
+  } else if (_UsePaddingValue) {
+    padmode = 2;
+  }
+
+  // Convolve along x axis
+  if (_KernelX && input->X() > 1) {
+    if (output == this->Input()) {
+      output = new ImageType(attr);
+    }
+    for (int n = 0; n < N; ++n) {
+      switch (padmode) {
+        case 1: {
+          typedef ConvolutionFunction::ConvolveForegroundInX<TKernel> ConvFunc;
+          ConvFunc conv(input, _KernelX->Data(), _KernelX->X(), _Normalize, n);
+          ParallelForEachVoxel(attr, input, output, conv);
+        } break;
+        case 2: {
+          typedef ConvolutionFunction::ConvolveTruncatedForegroundInX<TKernel> ConvFunc;
+          ConvFunc conv(input, padding, _KernelX->Data(), _KernelX->X(), _Normalize, n);
+          ParallelForEachVoxel(attr, input, output, conv);
+        } break;
+        default: {
+          typedef ConvolutionFunction::ConvolveInX<TKernel> ConvFunc;
+          ConvFunc conv(input, _KernelX->Data(), _KernelX->X(), _Normalize, n);
+          ParallelForEachVoxel(attr, input, output, conv);
+        } break;
+      }
+    }
+    swap(input, output);
+  }
+
+  // Convolve along y axis
+  if (_KernelY && input->Y() > 1) {
+    if (output == this->Input()) {
+      output = new ImageType(attr);
+    }
+    for (int n = 0; n < N; ++n) {
+      switch (padmode) {
+        case 1: {
+          typedef ConvolutionFunction::ConvolveForegroundInY<TKernel> ConvFunc;
+          ConvFunc conv(input, _KernelY->Data(), _KernelY->X(), _Normalize, n);
+          ParallelForEachVoxel(attr, input, output, conv);
+        } break;
+        case 2: {
+          typedef ConvolutionFunction::ConvolveTruncatedForegroundInY<TKernel> ConvFunc;
+          ConvFunc conv(input, padding, _KernelY->Data(), _KernelY->X(), _Normalize, n);
+          ParallelForEachVoxel(attr, input, output, conv);
+        } break;
+        default: {
+          typedef ConvolutionFunction::ConvolveInY<TKernel> ConvFunc;
+          ConvFunc conv(input, _KernelY->Data(), _KernelY->X(), _Normalize, n);
+          ParallelForEachVoxel(attr, input, output, conv);
+        } break;
+      }
+    }
+    swap(input, output);
+  }
+
+  // Convolve along z axis
+  if (_KernelZ && input->Z() > 1 && !AreEqual(input->ZSize(), 0.)) {
+    if (output == this->Input()) {
+      output = new ImageType(attr);
+    }
+    for (int n = 0; n < N; ++n) {
+      switch (padmode) {
+        case 1: {
+          typedef ConvolutionFunction::ConvolveForegroundInZ<TKernel> ConvFunc;
+          ConvFunc conv(input, _KernelZ->Data(), _KernelZ->X(), _Normalize, n);
+          ParallelForEachVoxel(attr, input, output, conv);
+        } break;
+        case 2: {
+          typedef ConvolutionFunction::ConvolveTruncatedForegroundInZ<TKernel> ConvFunc;
+          ConvFunc conv(input, padding, _KernelZ->Data(), _KernelZ->X(), _Normalize, n);
+          ParallelForEachVoxel(attr, input, output, conv);
+        } break;
+        default: {
+          typedef ConvolutionFunction::ConvolveInZ<TKernel> ConvFunc;
+          ConvFunc conv(input, _KernelZ->Data(), _KernelZ->X(), _Normalize, n);
+          ParallelForEachVoxel(attr, input, output, conv);
+        } break;
+      }
+    }
+    swap(input, output);
+  }
+
+  // Convolve along t axis
+  if (_KernelT && input->T() > 1 && !AreEqual(input->TSize(), 0.)) {
+    if (output == this->Input()) {
+      output = new ImageType(attr);
+    }
+    switch (padmode) {
+      case 1: {
+        typedef ConvolutionFunction::ConvolveForegroundInT<TKernel> ConvFunc;
+        ConvFunc conv(input, _KernelT->Data(), _KernelT->X(), _Normalize);
+        ParallelForEachVoxel(attr, input, output, conv);
+      } break;
+      case 2: {
+        typedef ConvolutionFunction::ConvolveTruncatedForegroundInT<TKernel> ConvFunc;
+        ConvFunc conv(input, padding, _KernelT->Data(), _KernelT->X(), _Normalize);
+        ParallelForEachVoxel(attr, input, output, conv);
+      } break;
+      default: {
+        typedef ConvolutionFunction::ConvolveInT<TKernel> ConvFunc;
+        ConvFunc conv(input, _KernelT->Data(), _KernelT->X(), _Normalize);
+        ParallelForEachVoxel(attr, input, output, conv);
+      } break;
+    }
+    swap(input, output);
+  }
+
+  // Copy result if last output (i.e., after swap input pointer) was not filter
+  // output image and delete possibly additionally allocated image
+  if (input != output) {
+    if (input == this->Output()) {
+      if (output != this->Input()) {
+        Delete(output);
+      }
+    } else {
+      this->Output()->CopyFrom(input->Data());
+      if (input != this->Input()) {
+        Delete(input);
+      }
+    }
+  }
+
+  // Do the final cleaning up
+  this->Finalize();
+
+  MIRTK_DEBUG_TIMING(5, this->NameOfClass());
+}
+
+// -----------------------------------------------------------------------------
+template <class TVoxel, class TKernel>
+void SeparableConvolution<TVoxel, TKernel>::Finalize()
+{
+  // Finalize base class
+  ImageToImage<TVoxel>::Finalize();
+
+  // Copy background information
+  if (this->Input() != this->Output()) {
+    if (this->Input()->HasBackgroundValue()) {
+      this->Output()->PutBackgroundValueAsDouble(this->Input()->GetBackgroundValueAsDouble());
+    }
+    if (this->Input()->HasMask()) {
+      this->Output()->PutMask(new BinaryImage(*this->Input()->GetMask()), true);
+    }
+  }
+}
+
+// -----------------------------------------------------------------------------
+template <class TVoxel, class TKernel>
+void SeparableConvolution<TVoxel, TKernel>::RunX()
+{
+  const KernelType * const ky = _KernelY;
+  const KernelType * const kz = _KernelZ;
+  const KernelType * const kt = _KernelT;
+  _KernelY = _KernelZ = _KernelT = nullptr;
+
+  this->Run();
+
+  _KernelY = ky;
+  _KernelZ = kz;
+  _KernelT = kt;
+}
+
+// -----------------------------------------------------------------------------
+template <class TVoxel, class TKernel>
+void SeparableConvolution<TVoxel, TKernel>::RunY()
+{
+  const KernelType * const kx = _KernelX;
+  const KernelType * const kz = _KernelZ;
+  const KernelType * const kt = _KernelT;
+  _KernelX = _KernelZ = _KernelT = nullptr;
+
+  this->Run();
+
+  _KernelX = kx;
+  _KernelZ = kz;
+  _KernelT = kt;
+}
+
+// -----------------------------------------------------------------------------
+template <class TVoxel, class TKernel>
+void SeparableConvolution<TVoxel, TKernel>::RunZ()
+{
+  const KernelType * const kx = _KernelX;
+  const KernelType * const ky = _KernelY;
+  const KernelType * const kt = _KernelT;
+  _KernelX = _KernelY = _KernelT = nullptr;
+
+  this->Run();
+
+  _KernelX = kx;
+  _KernelY = ky;
+  _KernelT = kt;
+}
+
+// -----------------------------------------------------------------------------
+template <class TVoxel, class TKernel>
+void SeparableConvolution<TVoxel, TKernel>::RunT()
+{
+  const KernelType * const kx = _KernelX;
+  const KernelType * const ky = _KernelY;
+  const KernelType * const kz = _KernelZ;
+  _KernelX = _KernelY = _KernelZ = nullptr;
+
+  this->Run();
+
+  _KernelX = kx;
+  _KernelY = ky;
+  _KernelZ = kz;
+}
+
+// =============================================================================
+// Explicit template instantiations
+// =============================================================================
+
+template class SeparableConvolution<unsigned char, float>;
+template class SeparableConvolution<short, float>;
+template class SeparableConvolution<unsigned short, float>;
+template class SeparableConvolution<float, float>;
+template class SeparableConvolution<double, float>;
+
+template class SeparableConvolution<unsigned char, double>;
+template class SeparableConvolution<short, double>;
+template class SeparableConvolution<unsigned short, double>;
+template class SeparableConvolution<float, double>;
+template class SeparableConvolution<double, double>;
+
+
+} // namespace mirtk
