42fa23b1967dc74e68b4703d5e64946f98c8fe4a
Surui Li <lisurui6@gmail.com>
2020-09-23 18:57:41 +0100

    update license

--- Applications/src/detect-cardiac-phases.cc
+++ Applications/src/detect-cardiac-phases.cc
@@ -2,8 +2,6 @@
  * Medical Image Registration ToolKit (MIRTK)
  *
  * Copyright 2008-2015 Imperial College London
- * Copyright 2008-2013 Daniel Rueckert, Julia Schnabel
- * Copyright 2013-2015 Andreas Schuh
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
